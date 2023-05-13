69f0f376e90a8b1037cb984203fca305a669cc09
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-05-13 20:28:57 +0100

    portfolio

---
+++ portfolio-site/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+export default function NavBar() {
+  return (
+    <div>
+        <header>
+        <img src="./images/Logo-fotor-bg-remover-2023051319917.png" alt="logo" height="150"  width="180"/>
+      <nav>
+        <ul>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">My Works</a></li>
+            <li><a href="#">Contact Me</a></li>
+        </ul>
+      </nav>
+      </header>
+    </div>
+  )
+}
